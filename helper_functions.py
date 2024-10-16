607a08a32ed349f853383c0718aaac7f5274c9bc
Kanza Nasim <124770593+KanzaNAsim@users.noreply.github.com>
2024-10-16 20:52:38 +0500

    created some helper functions

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,35 @@
-# count the characters
+# function to generate random even number
+import random
 
-name1= input("write your name here...")
-count= len(name1)
-print("there are",count,"characters in your name!")
+def generate_random_even_number():
+    """Generates a random even number.
+
+    Returns:
+        int: A random even number.
+    """
+
+    while True:
+        number = random.randint(0, 50) 
+        if number % 2 == 0:
+            return number
+           
+        
+rnumber = generate_random_even_number()
+
+
+
+# function to calculate half of a random even number
+def calculate_half(rnumber):
+    """Calculates half of a random even number.
+
+    Args:
+        random_even_number (int): The random even number.
+
+    Returns:
+        int: Half of the random even number.
+    """
+
+    return rnumber // 2
+
+
+hnumber = calculate_half(rnumber)
