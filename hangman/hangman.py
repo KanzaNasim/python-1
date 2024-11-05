880c6676d7645c46085dc338a0cd7fbb999dd443
KanzaNasim <k.nasim.27259@khi.iba.edu.pk>
2024-11-05 11:23:09 +0500

    # made minor changs for better user experience.

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -8,11 +8,11 @@ word = random.choice(word_list).lower()
 guess_list = ["_"] * len(word)
 
 
-wrong_guesses = 0
+wrong_guesses = 6
 
 while True:
     guess = input(f"Guess the letters (there are {len(word)} letters, write them one at a time): {''.join(guess_list)} ").lower()
-    
+
     if guess in word:
         print("Nice guess!")
         for i, letter in enumerate(word):
@@ -21,20 +21,38 @@ while True:
         print(f"It is one of the letters: {''.join(guess_list)}")
     else:
         print("Wrong guess!")
-        print(stages[wrong_guesses])
+        print(stages[6 - wrong_guesses])
+        print("*************************")
+        print(f"You have {wrong_guesses} lives remaining.")
+        print("*************************")
         print(hint[wrong_guesses % len(hint)])
-        wrong_guesses += 1
-        if wrong_guesses == len(stages):
-            print("Game over!")
-            break
-    
+        wrong_guesses -= 1
+
+        if wrong_guesses == 0:
+            print("You have 1 life remaining. I'll give you one more chance!")
+            guess = input(f"Guess the letters (there are {len(word)} letters, write them one at a time): {''.join(guess_list)} ").lower()
+            if guess in word:
+                print("Nice guess!")
+                for i, letter in enumerate(word):
+                    if letter == guess:
+                        guess_list[i] = guess
+                print(f"It is one of the letters: {''.join(guess_list)}")
+            else:
+                print("Wrong guess!")
+                print(stages[6])
+                print("*************************")
+                print("Game over!")
+                print(f"The word was: {word}")
+                break
+
     if "".join(guess_list) == word:
         print("Yay, you did it!")
         y = input("Wanna play again? (y or n): ").lower()
         if y == 'y':
             word = random.choice(word_list).lower()
             guess_list = ["_"] * len(word)
-            wrong_guesses = 0
+            wrong_guesses = 6
         else:
             print("Okay, thanks for playing!")
             break
+
